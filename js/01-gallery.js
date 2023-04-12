5820f78af8aac02090ff2dbde827a57dfa827541
Oleksandr Varga <vargasasha@gmail.com>
2023-04-12 14:44:01 +0300

    add markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createMarkup(array) {
+    const markup = array
+        .map(
+            ({
+                preview,
+                original,
+                description,
+            }) => `<li class="gallery__item"><a class="gallery__link" href=${original}><img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>`
+        )
+        .join("");
+
+    return markup;
+}
+
+gallery.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
